53f9e98f548c6fcc21ce7ddc462b206307823cdd
Npvpr <nlmnever2@gmail.com>
2024-05-28 11:54:03 +0700

    First Setup

---
+++ src/test/java/SpringLearn/taco_cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringLearn.taco_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
